e0ac9185c1b159ffda52765f7e3767d7ce762ee0
Igor Srdoč <igor@srdoc.si>
2018-03-20 08:19:28 +0100

    add CORS and test data call from frontend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Simple exchange';
+export class AppComponent implements OnInit, OnDestroy {
+
+  private title: string;
+  private subscription: Subscription;
+  private urlServer = 'http://localhost:6543';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getData();
+  }
+
+  getData() {
+    this.subscription = this.http.get(this.urlServer)
+      .map(data => data ? data : null)
+      .subscribe(result => this.title = result['project']);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
 }
